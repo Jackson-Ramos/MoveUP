683618c631cbe23824156cfd2636c75bd24dd05c
Jackson Ramos <jackson.souzza.r@gmail.com>
2025-07-31 20:58:59 -0300

    FEAT: Criação do formulário do aluno

--- frontend_MoveUp/src/App.tsx
+++ frontend_MoveUp/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
+import StudentProfile from './components/student_profile/Student-profile'
 
 function App() {
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
   {/* <Login/> */}
-  <Register/>
+  {/* <Register/> */}
+  <StudentProfile/>
     </>
   )
 }
---
+++ frontend_MoveUp/src/components/student_profile/Student-profile.tsx
@@ -0,0 +1,20 @@
+import './student_profile.css'
+
+function StudentProfile() {
+    return (
+        <>
+
+            <section id="container">
+
+                <form action="">
+
+                    <input type="text" name="goals" id="goals" placeholder='Objetivo' />
+                </form>
+
+            </section>
+
+        </>
+    )
+}
+
+export default StudentProfile
