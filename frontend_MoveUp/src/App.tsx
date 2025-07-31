c1c65cce27a83b29d27ca5dd70823beec7968837
Jackson Ramos <jackson.souzza.r@gmail.com>
2025-07-31 20:41:24 -0300

    FEAT: tela de cadastro.

--- frontend_MoveUp/src/App.tsx
+++ frontend_MoveUp/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Login from './components/login/Login'
+import Register from './components/register/Register'
 
 function App() {
 
 
   return (
     <>
-  <Login/>
+  {/* <Login/> */}
+  <Register/>
     </>
   )
 }
---
+++ frontend_MoveUp/src/components/register/Register.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './register.css'
+
+function Register() {
+  return (
+    <>
+    <div className='container'>
+      <section className='login'>
+        <h1>Entrar</h1>
+        <p>Já Possui uma conta</p>
+        <button>Entrar</button>
+      </section>
+
+      <section className='register'>
+        <h1 className='title'>Cadastre-se</h1>
+
+        <form >
+          <input
+            className='input-user input-register'
+            type='text'
+            placeholder='Nome'
+            required
+          />
+
+          <input
+            className='input-email input-register'
+            type='email'
+            placeholder='Email'
+            required
+          />
+
+          <input
+            className='input-password input-register'
+            type='password'
+            placeholder='Senha'
+            required
+          />
+
+          <button id='send' type='submit'>
+            Enviar
+          </button>
+        </form>
+      </section>
+      </div>
+    </>
+  )
+}
+
+export default Register
