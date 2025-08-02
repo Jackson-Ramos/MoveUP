974ec6679942d1947f07e29bafda01113c1e1a21
Jackson Ramos <jackson.souzza.r@gmail.com>
2025-08-01 21:44:53 -0300

    REFACTOR: Continuação da implementação do formulário do aluno

--- frontend_MoveUp/src/components/student_profile/Student-profile.tsx
+++ frontend_MoveUp/src/components/student_profile/Student-profile.tsx
@@ -8,7 +8,42 @@ function StudentProfile() {
 
                 <form action="">
 
-                    <input type="text" name="goals" id="goals" placeholder='Objetivo' />
+                    <section className='section_one'>
+                        <label htmlFor="goals">Objetivo:
+                            <input type="text" name="goals" id="goals" placeholder='Objetivo' />
+                        </label>
+                    </section>
+
+                    <section id='section_two'>
+
+                        <label htmlFor="birthDate">
+                            Data de nascimento:
+                            <input type="date" name="birthDate" id="birthDate" />
+                        </label>
+
+                        <label htmlFor="gender">Gênero:
+                            <select name="gender" id="gender">
+                                <option value="">Seleciona</option>
+                                <option value="masculine">Masculino</option>
+                                <option value="feminine">Feminino</option>
+                                <option value="other">Outro</option>
+                            </select>
+                        </label>
+
+                        <label htmlFor="currentHeight">Altura:
+                            <input type="number" name="currentHeight" id="currentHeight" />
+                        </label>
+
+                        <label htmlFor="currentWeight">Peso:
+                            <input type="number" name="currentWeight" id="currentWeight" />
+                        </label>
+                    </section>
+
+                    <section id="section_three">
+                        <label htmlFor="limitations">Possui alguma limitação?
+                            <textarea name="limitations" id="limitations"></textarea>
+                        </label>
+                    </section>
                 </form>
 
             </section>
